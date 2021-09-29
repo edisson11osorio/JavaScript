cd3209f869ba8893f6fd4622206f4cae219a4ada
edisson <osorioedisson08@gmail.com>
2021-09-29 11:32:56 -0500

    [ADD]var.js

---
+++ variables.js
@@ -0,0 +1,11 @@
+var nombre;
+var pais;
+var edad;
+
+nombre = prompt("escriba su nombre");
+pais = prompt("escriba su pais");
+edad = prompt("escriba su edad")
+
+document.write(nombre + " " + edad);
+
+alert("no coinciden")
